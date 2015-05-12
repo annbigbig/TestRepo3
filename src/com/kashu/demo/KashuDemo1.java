495fb80278adf2ff721f5500bb984b75355d80d9
KunAn Hsu <annbigbig@gmail.com>
2015-05-12 11:03:24 +0800

    KashuDemo1.java modified [1]

--- src/com/kashu/demo/KashuDemo1.java
+++ src/com/kashu/demo/KashuDemo1.java
@@ -3,7 +3,7 @@ package com.kashu.demo;
 public class KashuDemo1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	  System.out.println("Tony Stark is here!");
 
 	}
 
